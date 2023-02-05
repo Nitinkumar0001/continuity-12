4951e95a199fe730cf4c4a027efe9ea099b265b3
nitin kumar <124438805+Nitinkumar0001@users.noreply.github.com>
2023-02-06 02:03:55 +0530

    Add files via upload

---
+++ 12switch.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int main() {
+
+	// Switch Statements
+	// 0: 	Very poor
+	// 1: 	Poor
+	// 2: 	Average
+	// 3: 	Good
+	// 4,5: Excellent
+
+	int rating;
+	puts("How likely you would like to rate our pizza on a scale of five?");
+	puts("Reply with a number from 0 to 5, where 0 is least likely and 5 is most likely.");
+
+	puts("Enter your rating: ");
+	scanf("%d", &rating);
+
+	switch (rating) {
+
+		case 0:
+			puts("You had very poor experience. We will call you shortly to know what went wrong.");
+			break;
+		case 1:
+			puts("You had poor experience. We will call you shortly to know what went wrong.");
+			break;
+		case 2:
+			puts("Thank you for your feedback. We will call you shortly to know what went wrong.");
+			break;
+		case 3:
+			puts("Thank you for your feedback. We're constantly working to improve pizza quality.");
+			break;
+		case 4:
+        puts("Yay! Thank you for your feedback. We would try to improve that 1% also next time.");
+			break;
+		case 5:
+			puts("Yay! Thank you for your feedback. We would love to serve you again.");
+			break;
+		default:
+			puts("Invalid input.");
+	}
+
+	return 0;
+}
